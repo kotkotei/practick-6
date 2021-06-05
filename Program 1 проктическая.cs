12ae70b8508aa1a9fc1729b4a7a051486e4bdef2
kotkotei <kotkotei@yandex.ru>
2021-06-05 09:24:41 +0700

    Ti PROSTO PROSTO PUIIIKA

---
+++ Program 1 проктическая.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] mas = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(-50, 100);
+                Console.WriteLine(mas[i]);
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0)
+                {
+                    mas[i] = mas[i] + 100;
+                }
+                else
+                {
+                    mas[i] = mas[i] - 50;
+                }
+                Console.WriteLine(mas[i]);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program2 заданрие.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+            double[] mas = new double[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(22,100 )/10.0;
+                Console.WriteLine(mas[i]);
+        }
+            Console.ReadKey();
+        }
+    }
+}
